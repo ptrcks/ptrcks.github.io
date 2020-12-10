939baa2f52bdbc73f5e2a30ab9de372fe1c224f9
ptrcks <75756786+ptrcks@users.noreply.github.com>
2020-12-10 11:21:57 +0100

    Update rockpaperscissors.js

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -7,54 +7,88 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
+function convert(choice){
+    switch(choice){
+    case "r":
+        return "Rock"
+    case "p":
+        return "Paper"
+    case "s":
+        return "Scissors"
+ 
+    }
+}
+ 
 
-function win(){
+function win(comp, user){
     UserScore += 1
     UserScore_span.innerHTML = UserScore
-    Annoncement_p.innerHTML = "You Win"
+    comp = convert(comp)
+    user = convert(user)
+    Annoncement_p.innerHTML = user + " beats " + comp + ". You Win!"
 }
-
-function loose(){
+ 
+function loose(comp, user){
     Compscore++
     CompScore_span.innerHTML = Compscore
-    Annoncement_p.innerHTML = "You Loose"
+    comp = convert(comp)
+    user = convert(user)
+    Annoncement_p.innerHTML = comp + " beats " + user + ". You Loose!"
+    
 }
-
-function draw(){
-    Annoncement_p.innerHTML = "Its a draw"
-
+ 
+function tie(comp){
+    comp = convert(comp)
+    Annoncement_p.innerHTML = "You both choose " + comp + ". Its a tie!"
+ 
 }
-
+function untoggle(){
+    document.getElementById(choice).classList.toggle("active")
+}
+ 
 function Compchoice(){
     let choices = ["r","p","s"]
     choice = choices[Math.floor(Math.random() * 3)]
+    document.getElementById(choice).classList.toggle("active")
+    setTimeout(untoggle, 1000)
     return choice
 }
+
 
 
 function compare(userChoice){
     let compChoice = Compchoice()
     switch(compChoice+userChoice){
         case "rp":
+            win("r","p")
+            break
         case "sr":
+            win("s","r")
+            break
         case "ps":
-            win()
-            console.log("win")
+            win("p","s")
             break;
         case "pr":
+            loose("p","r")
+            break
         case "rs":
+            loose("r","s")
+            break
         case "sp":
-            loose()
-            console.log("loose")
+            loose("s","p")
             break;
         case "rr":
+            tie("r")
+            break
         case "pp":
+            tie("p")
+            break
         case "ss":
-            draw()
-            console.log("draw")
-        break;
+            tie("s")
+            break;
     }
 }
+ 
 
 
 function tracker(){
